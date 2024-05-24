4479cb041810df360ab8e469452161f7076441c9
jaemimah <jemimah.odutola@gmail.com>
2024-05-23 18:34:21 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,14 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-        for (j = 0; j <= 9; j++)
+	for (j = 0; j <= 9; j++)
 	{
 		product = i * j;
-            
+
 	if (j != 0)
 		{
 		_putchar(',');
- 		_putchar(' ');
+		_putchar(' ');
 		}
 
 	if (product < 10 && j != 0)
@@ -33,7 +33,7 @@ void times_table(void)
 		{
 		_putchar(product + '0');
 		}
-        }
+	}
 	_putchar('\n');
 	}
 }
